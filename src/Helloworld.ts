8007eea906f3b21b7d63787f6f64b50152d9c28e
Chow Chun Kit <65175148+tim-ck@users.noreply.github.com>
2023-11-03 10:39:08 +0800

    Delete src directory

--- src/Helloworld.ts
+++
@@ -1,5 +0,0 @@
-function printStatusCode(code: string | number) {
-    console.log(`My status code is ${(code as string).toUpperCase()}.`)
-  }
-let message:string|number = "HelloWorld";
-console.log(message.toUpperCase());
